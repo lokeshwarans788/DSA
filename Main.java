58ed943df6b432ddd7c660b8c24e44cd98ac8d0e
lokeshwarans788 <lokeshwarans788@gmail.com>
2026-07-26 18:51:43 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+
+public class Main{
+    public static void main(String args[]) {
+        int arr[]=new int[5];
+        arr[0]= 10;
+        arr[1]= 20;
+        arr[2]= 30;
+        arr[3]= 40;
+
+        System.out.print(0);
+        System.out.print(1);
+    }
+}
